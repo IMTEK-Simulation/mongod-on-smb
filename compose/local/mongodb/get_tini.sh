2936616f1a0677ae34fd249cef03c29d4a02db69
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2020-05-22 19:53:09 +0200

    WIP: inster TINI

---
+++ compose/local/mongodb/get_tini.sh
@@ -0,0 +1,3 @@
+TINI_VERSION=v0.19.0
+curl -L -o tini https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini
+curl -L -o tini.asc https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini.asc
