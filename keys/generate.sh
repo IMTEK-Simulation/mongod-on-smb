a3907b3d6555c08b308583a63b7a756efc080085
Johannes Laurin Hörmann <johannes.hoermann@imtek.uni-freiburg.de>
2020-05-23 00:10:09 +0200

    WIP: certificates for multiple hosts

--- keys/generate.sh
+++ keys/generate.sh
@@ -1,24 +1,38 @@
+#!/bin/bash
+set -x
 # generate root certificate and derived set of self-signed certificates for testing purposes
 
-# source https://medium.com/@rajanmaharjan/secure-your-mongodb-connections-ssl-tls-92e2addb3c89
-temp_subj="/C=DE/ST=Some-State/L=Some-City/O=Some-Organization/OU=Some-Department/emailAddress=none@none.com"
+# sources:
+# - https://medium.com/@rajanmaharjan/secure-your-mongodb-connections-ssl-tls-92e2addb3c89
+# - http://apetec.com/support/GenerateSAN-CSR.htm
+temp_subj="/C=DE/ST=Baden-Wuerttemberg/L=Freiburg i. Br./O=University of Freiburg/OU=IMTEK Simulation/emailAddress=johannes.hoermann@imtek.uni-freiburg.de"
 root_subj="$temp_subj"
 
 subj_suffix_arr=( "/CN=mongodb" "/CN=mongo-express" "/CN=mongo-express" )
 subdir_arr=( "mongodb" "mongo_express_inwards" "mongo_express_outwards" )
 
+PASSW=$(openssl rand -base64 32)
+echo "$PASSW" > passw
+
 # generate and self-sign root certificate
 openssl genrsa -out rootCA.key 2048
-openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 1024 -out rootCA.pem -subj "$root_subj"
+openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 1024 -out rootCA.pem -config openssl.cnf -subj "${root_subj}" -batch
 
 for ((i=0;i<${#subj_suffix_arr[@]};++i)); do
     subj="${temp_subj}${subj_suffix_arr[i]}"
     subdir="${subdir_arr[i]}"
 
-    # generate mongodb key and self-signed certificate
     mkdir -p "${subdir}"
-    openssl genrsa -out "${subdir}"/tls_key.pem 2048
-    openssl req -new -key "${subdir}"/tls_key.pem -out "${subdir}"/tls_cert.csr -subj "${subj}"
-    openssl x509 -req -in "${subdir}"/tls_cert.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out "${subdir}"/tls_cert.pem -days 500 -sha256
-    cat "${subdir}"/tls_key.pem "${subdir}"/tls_cert.pem > "${subdir}"/tls_key_cert.pem
+    # generate key
+    openssl genrsa -out "${subdir}/tls_key.pem" 2048
+    # generate certificate request
+    openssl req -new -key "${subdir}/tls_key.pem" -out "${subdir}/tls_cert.csr" -config openssl.cnf -subj "${subj}" -batch
+    # print request to stdout
+    openssl req -text -noout -in "${subdir}/tls_cert.csr"
+    # generate self-signed certifictae
+    openssl x509 -req -in "${subdir}/tls_cert.csr" -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out "${subdir}/tls_cert.pem" -days 500 -sha256 -extensions v3_req -extfile openssl.cnf
+    # concatenate key and signed certificate in simple file
+    cat "${subdir}"/tls_key.pem "${subdir}/tls_cert.pem" > "${subdir}/tls_key_cert.pem"
+    # concatenate key and signed certificate in p12 file
+    openssl pkcs12 -export -in "${subdir}/tls_cert.pem" -inkey "${subdir}/tls_key_cert.pem" -out "${subdir}/tls_key_cert.p12" -password pass:$PASSW
 done
